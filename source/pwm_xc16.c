ddb97629c14f69c65b68eb3b907a51142358e376
Liam Bucci <controls.freq@gmail.com>
2014-08-12 15:55:53 -0700

    PWM
    == PWM ==
    - Added the XC16 versions of the PWM module (both header and source files).
    - Header file contains first draft of PWM interface, including attr struct, module struct, and global struct, as well as required enumerations.

---
+++ source/pwm_xc16.c
@@ -0,0 +1,35 @@
+/* -*- mode: C; tab-width: 4; -*- */
+
+/**
+ * @file pwm_xc16.c
+ *
+ * @brief This file contains the private implementations of the PWM module for the XC16 compiler.
+ *
+ * @details Nothing here.
+ *
+ * @date 2014/08/08
+ * @author Liam Bucci
+ * @carlnumber FIRM-0009
+ * @version 0.4.0
+ *
+ * @private
+ */
+
+/**
+ * @addtogroup pwm_xc16
+ *
+ * @private
+ *
+ * @{
+ */
+
+
+
+
+
+
+
+
+/**
+ * @}
+ */ // End of group pwm_xc16
