bdcc2fbea3b418c418a5c0c3eb99f452f4e1f36a
Liam Bucci <controls.freq@gmail.com>
2014-08-26 14:39:26 -0700

    Summary: Added UART unit test.
    Added uart_unit.c which contains some testing of the UART module.

---
+++ test/uart_unit.c
@@ -0,0 +1,32 @@
+/* -*- mode: C; tab-width: 4 -*- */
+/**
+ * @file uart_unit.c
+ *
+ * @brief This file contains all the unit tests for the UART module.
+ *
+ * @author Liam Bucci
+ * @date 6/16/2014
+ * @carlnumber FIRM-0009
+ * @version 0.4.0
+ */
+
+/**
+ * @addtogroup uart_module
+ *
+ * @{
+ */
+
+#include "../include/uart.h"
+
+
+void main(void)
+{
+    
+}
+
+
+
+
+/**
+ * @}
+ */
